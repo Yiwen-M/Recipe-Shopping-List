cce30802b322c9710bdad3d89e4891f898a46add
yww0225 <yw0225.career@gmail.com>
2022-07-19 16:27:59 -0700

    RSL-13 implemented post request to store recipes and get request to fetch recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,33 +9,40 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Butter Milk Fried Chicken',
-      'Super-crunchy fried chicken!',
-      'https://loveincorporated.blob.core.windows.net/contentimages/gallery/352faeb5-95f6-41e6-af4c-24ac02b46511-fried-chicken.jpg',
-      [
-        new Ingredient('Chicken Thigh', 6),
-        new Ingredient('Flour', 1),
-        new Ingredient('Buttermilk', 1),
-        new Ingredient('Egg', 2),
-      ]
-    ),
-    new Recipe(
-      'Cinnamon Roll',
-      'Fluffy roll with cinnamon filling, buttery enough to satisfy your deepest cravings!',
-      'https://a.cdn-hotels.com/gdcs/production87/d1511/bb6fc275-5cbf-40b4-9096-21730304c6a5.jpg?impolicy=fcrop&w=800&h=533&q=medium.jpg',
-      [
-        new Ingredient('Flour', 1),
-        new Ingredient('Cinnamon', 1),
-        new Ingredient('Sugar', 1),
-        new Ingredient('Butter', 1),
-        new Ingredient('Egg', 2),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Butter Milk Fried Chicken',
+  //     'Super-crunchy fried chicken!',
+  //     'https://loveincorporated.blob.core.windows.net/contentimages/gallery/352faeb5-95f6-41e6-af4c-24ac02b46511-fried-chicken.jpg',
+  //     [
+  //       new Ingredient('Chicken Thigh', 6),
+  //       new Ingredient('Flour', 1),
+  //       new Ingredient('Buttermilk', 1),
+  //       new Ingredient('Egg', 2),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Cinnamon Roll',
+  //     'Fluffy roll with cinnamon filling, buttery enough to satisfy your deepest cravings!',
+  //     'https://a.cdn-hotels.com/gdcs/production87/d1511/bb6fc275-5cbf-40b4-9096-21730304c6a5.jpg?impolicy=fcrop&w=800&h=533&q=medium.jpg',
+  //     [
+  //       new Ingredient('Flour', 1),
+  //       new Ingredient('Cinnamon', 1),
+  //       new Ingredient('Sugar', 1),
+  //       new Ingredient('Butter', 1),
+  //       new Ingredient('Egg', 2),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
