f731b48fa7f4c1cd33b304a76b4a26651c13e291
yww0225 <yw0225.career@gmail.com>
2022-07-19 18:13:10 -0700

    RSL-16 implemented signup and login post request and added error handling logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 
 import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeService } from './recipes/recipe.service';
@@ -33,6 +34,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     ShoppingListComponent,
     ShoppingEditComponent,
     AuthComponent,
+    LoadingSpinnerComponent,
     DropdownDirective,
   ],
   imports: [
